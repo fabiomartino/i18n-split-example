c0b3107615f3d1ae7e5eb9c50e6fdc63fff6c890
Fabio Martino <martino.fabio@libero.it>
2020-12-20 00:22:46 +0100

    wip update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterState, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { I18nModule } from './i18n/i18n.module';
 import { metaReducers, ROOT_REDUCERS } from './reducers';
 
-export function createTranslateLoader(http: HttpClient): any {
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/app/', '.json');
 }
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [ AppComponent ],
   imports: [
     BrowserModule,
     HttpClientModule,
     I18nModule,
-    AppRoutingModule,
     StoreModule.forRoot(ROOT_REDUCERS, {
       metaReducers,
       runtimeChecks: {
@@ -38,17 +37,17 @@ export function createTranslateLoader(http: HttpClient): any {
     StoreDevtoolsModule.instrument({
       name: 'NgRx Book Store App'
     }),
-
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: createTranslateLoader,
-        deps: [HttpClient]
+        deps: [ HttpClient ]
       },
       isolate: true
-    })
+    }),
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
